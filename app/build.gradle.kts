1609e6985b0bb6e016f60c60801f416ad0994346
Riganji <riganji@mail.ru>
2026-09-20 16:20:30 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -24,7 +26,7 @@ android {
      */
     signingConfigs {
         create("app") {
-            val props = java.util.Properties().apply {
+            val props = Properties().apply {
                 val f = rootProject.file("keystore.properties")
                 if (f.exists()) f.inputStream().use { load(it) }
             }
