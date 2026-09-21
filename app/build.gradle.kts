81df385c345093bbddd73d361dd4242f7886351c
Riganji <riganji@mail.ru>
2026-09-21 21:56:34 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
         // versionName сравнивается с тегом релиза на GitHub (см. Updater.kt),
         // поэтому тег релиза должен быть «v<versionName>», например v0.3.0.
         versionCode = 6
-        versionName = "0.3.0"
+        versionName = "0.3.1"
 
         buildConfigField("String", "UPDATE_REPO", "\"${setting("updateRepo", "UPDATE_REPO", "Riganji/TTV")}\"")
         buildConfigField("String", "UPDATE_TOKEN", "\"${hideToken(setting("updateToken", "UPDATE_TOKEN", ""))}\"")
